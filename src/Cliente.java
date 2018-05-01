d8c143921e38af1b25a4aaff5dc4a8471cb234da
Jaime andres <Jaime andres@LAPTOP-B3RPBKKV>
2018-04-30 20:05:14 -0500

    se creo crearon metodos de listar y consultar cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -5,13 +5,24 @@ import org.omg.CORBA.ORB;
 public class Cliente extends ClienteApp.ClientePOA {
     
     private ORB orb;
+    Conexion conex = new Conexion();
     
     @Override
     public boolean insertarCliente(int CL_ID, String CL_NOMBRE, String CL_DIRECCION, int CL_TEL, String CL_ESTADO) {
       boolean resultado=false;
         try {
+            
             String query ="insert into Cliente(CL_ID,CL_NOMBRE,CL_DIRECCION,CL_TEL,CL_ESTADO)"
-            + "value("+ CL_ID+","+CL_NOMBRE+","+CL_DIRECCION+","+CL_TEL+","+CL_ESTADO+")";
+            + "value('"+ CL_ID+"','"+CL_NOMBRE+"','"+CL_DIRECCION+"','"+CL_TEL+"','"+CL_ESTADO+"')";
+            conex.conexion();
+            Statement st = conex.conex.createStatement();
+            int valor =st.executeUpdate(query);
+            if (valor >0){
+                resultado=true;
+                JOptionPane.showMessageDialog(null, "datos insertados correctamente");
+            }
+            st.close();
+            conex.conex.close();
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Ocurrio un error"+e.getMessage());
         }
@@ -20,7 +31,29 @@ public class Cliente extends ClienteApp.ClientePOA {
 
     @Override
     public String consultarCliente(int CL_ID, String CL_NOMBRE, String CL_DIRECCION, int CL_TEL, String CL_ESTADO) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       String resultado ="";
+        
+        try {
+            String query="Select * from cliente WHERE CL_ID="+CL_ID;
+            conex.conexion();
+            Statement st = conex.conex.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            while(rs.next()){
+                resultado += rs.getLong(1)+ " - " 
+                        + rs.getString(2)+ " - "
+                        + rs.getString(3)+ " - "
+                        + rs.getLong(4)+ " - "
+                        + rs.getString(5)+ "\n";
+            }
+            st.close();
+            rs.close();
+            conex.conex.close();
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"ocurrio un error"+e.getMessage());
+        }
+        return resultado;
+    
     }
 
     @Override
@@ -35,7 +68,28 @@ public class Cliente extends ClienteApp.ClientePOA {
 
     @Override
     public String listarCliente() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String resultado ="";
+        
+        try {
+            String query="Select * from cliente";
+            conex.conexion();
+            Statement st = conex.conex.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            while(rs.next()){
+                resultado += rs.getLong(1)+ " - " 
+                        + rs.getString(2)+ " - "
+                        + rs.getString(3)+ " - "
+                        + rs.getLong(4)+ " - "
+                        + rs.getString(5)+ "\n";
+            }
+            st.close();
+            rs.close();
+            conex.conex.close();
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"ocurrio un error"+e.getMessage());
+        }
+        return resultado;
     }
 
     @Override
